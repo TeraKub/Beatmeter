0c47960b40e07faf5c4e4d1e9beedd4c45970916
TeraKub <68902968+TeraKub@users.noreply.github.com>
2024-01-09 10:46:18 +0300

    Update Scene1.js

--- assets/scenes/Scene1.js
+++ assets/scenes/Scene1.js
@@ -30,29 +30,20 @@ class Scene1 extends Phaser.Scene {
 		this.offsetPointY = 29;
 		this.offset16thNote = 38.7;
 		
-        this.line;
-        this.pointsGroup;
+        //this.line;
+        //this.pointsGroup;
         //this.score = 0;
         this.scoreText;
 
         // Создание группы для точек
-        this.pointsGroup = this.physics.add.group();
+        //this.pointsGroup = this.physics.add.group();
 
         // Текст для отображения счета
         this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 
-        // Кнопка "Старт"
-        /*this.startButton = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'СТАРТ', { fontSize: '32px', fill: '#fff' })
-            .setOrigin(0.5)
-            .setInteractive()
-            .on('pointerdown', () => {
-				this.start()
-			});*/
-
         // Кнопка "Тап"
         this.input.on('pointerdown', () => this.placePoint());
         
-        // Флаг для определения, идет ли игра
         this.isGameActive = false;
 
 		this.blackDotStartX = this.posX + this.offsetPointX - 23;
@@ -60,7 +51,6 @@ class Scene1 extends Phaser.Scene {
 		this.blacDotAndClickPointY = 700;
 		this.blackDot = this.add.circle(this.blackDotStartX, this.blacDotAndClickPointY, 10, this.backColor);
 		this.physics.add.existing(this.blackDot);
-		//console.log(this.blackDot.x, this.blackDot.y);
 		
 		this.startMenu();
 	}
@@ -68,11 +58,8 @@ class Scene1 extends Phaser.Scene {
 	/* START-USER-CODE */
 
 	create() {
-		this.backColor = '0x8C77E2';
-		//this.createNotes();
+		this.backColor = '0x8C77E20';
 		this._create();
-		//this.setCount = 0;
-		//this.createClickPoint();
 	}
 
 	update() {
@@ -89,49 +76,68 @@ class Scene1 extends Phaser.Scene {
             .on('pointerdown', () => {
 				this.complexityOfLevelNotes = 2;
 				this.bpm = 100;
+				this.level = 1;
 				this.start()
 			});
+		this.scoreLevel1 = this.add.text(this.game.config.width / 2 - offsetX, 130 + offsetY, '0', { fontSize: '40px', fill: '#fff' })
+            .setOrigin(0.5);
+
 		const levelButton2 = this.add.text(this.game.config.width / 2, 100 + offsetY, '11-20', { fontSize: '40px', fill: '#fff' })
             .setOrigin(0.5)
             .setInteractive()
             .on('pointerdown', () => {
 				this.complexityOfLevelNotes = 3;
 				this.bpm = 90;
+				this.level = 11;
 				this.start()
 			});
+		this.scoreLevel2 = this.add.text(this.game.config.width / 2, 130 + offsetY, '0', { fontSize: '40px', fill: '#fff' })
+            .setOrigin(0.5);
+
 		const levelButton3 = this.add.text(this.game.config.width / 2 + offsetX, 100 + offsetY , '21-30', { fontSize: '40px', fill: '#fff' })
             .setOrigin(0.5)
             .setInteractive()
             .on('pointerdown', () => {
 				this.complexityOfLevelNotes = 4;
 				this.bpm = 80;
+				this.level = 21;
 				this.start()
 			});
+		this.scoreLevel3 = this.add.text(this.game.config.width / 2 + offsetX, 130 + offsetY, '0', { fontSize: '40px', fill: '#fff' })
+            .setOrigin(0.5);
 		
 		this.levelButtons = [levelButton1, levelButton2, levelButton3];
+		this.scoreLevels = [this.scoreLevel1, this.scoreLevel2, this.scoreLevel3];
 	}
 	
 	start() {
 		for (var btn of this.levelButtons) {
 			btn.destroy();
 		}
-		this.startButton = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'СТАРТ', { fontSize: '32px', fill: '#fff' })
+		
+		for (var lvl of this.scoreLevels) {
+			lvl.visible = false;
+		}
+		
+		this.scoreText.visible = false;
+		this.levelText = this.add.text(this.game.config.width / 2, 16, 'Уровень: ' + this.level, 
+			{ fontSize: '32px', fill: '#fff' })
+			.setOrigin(0.5);
+			
+		this.startButton = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 
+			'СТАРТ', { fontSize: '32px', fill: '#fff' })
             .setOrigin(0.5)
             .setInteractive()
             .on('pointerdown', () => {
 				this.startGame()
 			});
 		this.score = 1;
-		//console.log('score', this.score);
 		this.correctScore = 1;
-		//console.log('correctScore', this.correctScore);
 		this.createNotes();
 		this.createClickPoint();
-		//this.startGame();
 	}
 	
 	startGame() {
-        // Убираем кнопку "Старт"
         this.startButton.destroy();
 
 		this.clickLamp = this.add.circle(
@@ -142,8 +148,7 @@ class Scene1 extends Phaser.Scene {
 			);
 
 		if (this.setCount == 2) {
-			// Включаем флаг игры
-        this.isGameActive = true;
+        	this.isGameActive = true;
 		}
 		
 		
@@ -153,7 +158,6 @@ class Scene1 extends Phaser.Scene {
             x: this.blackDotFinishX,
             duration: 60 / this.bpm * 4000,
             onComplete: () => {
-				//console.log(this.setCount);
 				this.setCount++;
 				if (this.setCount < 4) {
 	                this.createClickPoint();
@@ -170,28 +174,23 @@ class Scene1 extends Phaser.Scene {
 		this.numNotes = ['0000', '1000', '1010', '0010'];
 		this.randomNotes = [];
 		this.beats = [];
-		//console.log(this.numNotes);
-		
-		/*this.posX = 137;
-		this.posY = 150;
-		this.offsetBeat = 75;
-		this.offsetPic = 154.6;
-		this.offsetY = 200;*/
+
 		for (var i = 0; i < 4; i++) {
 			const beat = this.add.text(this.posX - this.offsetBeat + this.offsetPic * i, this.posY - 75, i + 1, { fontSize: '32px', fill: '#fff', fontStyle: 'bold' });
 			this.beats.push(beat);
 			this.add.image(this.posX + this.offsetPic * i, this.posY, 'textures', this.getRandomElement(this.numNotes)).setScale(0.5, 0.5);
 		}
+		
 		for (var i = 0; i < 4; i++) {
 			const beat = this.add.text(this.posX - this.offsetBeat + this.offsetPic * i, this.posY - 75 + this.offsetY, i + 1, { fontSize: '32px', fill: '#fff', fontStyle: 'bold' });
 			this.beats.push(beat);
 			this.add.image(this.posX + this.offsetPic * i, this.posY + this.offsetY, 'textures', this.getRandomElement(this.numNotes)).setScale(0.5, 0.5);
 		}
+		
 		this.createNotePoints();
 	}
 	
 	getRandomElement(arr) {
-		//const note = arr[Math.floor(Math.random() * arr.length)];
 		const note = arr[Math.floor(Math.random() * this.complexityOfLevelNotes)];
 		this.randomNotes.push(note);
 	    return note;
@@ -238,7 +237,7 @@ class Scene1 extends Phaser.Scene {
 			}
 			
 			const clickPoint = this.add.circle(
-				this.posX + this.offsetPointX + this.offsetPic * i + 10, 
+				this.posX + this.offsetPointX + this.offsetPic * i + 12, 
 				this.blacDotAndClickPointY, 10, this.backColor
 			);
 			this.physics.add.existing(clickPoint);
@@ -247,9 +246,7 @@ class Scene1 extends Phaser.Scene {
 	}
 	
 	click() {
-		if (this.blackDot.x > this.blackDotStartX && 
-		this.blackDot.x < this.posX + this.offsetPointX + this.offsetPic * 4 - this.offset16thNote
-		) {
+		if (this.blackDot.x > this.blackDotStartX && this.blackDotFinishX) {
 			for (const clickPoint of this.clickPoints) {
 				if (this.physics.overlap(clickPoint, this.blackDot)) {
 					clickPoint.destroy();
@@ -276,23 +273,19 @@ class Scene1 extends Phaser.Scene {
 	}
 
     endGame() {
-		//console.log(this.notePoints);
 		for (const point of this.notePoints) {
 			if (point.body) {
-				//console.log('ЛОХ');
 				this.correctScore = 0;
-				//console.log('correctScore', this.correctScore);
 			}
 		}
 	
-        // Отключаем флаг игры
         this.isGameActive = false;
 
 		this.scoreText.setText('Score: ' + this.score * this.correctScore);
-		//console.log('finaleScore', this.score * this.correctScore);
 
         // Выводим сообщение об окончании игры
-        this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'Game Over', { fontSize: '48px', fill: '#fff' })
+        this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'Game Over', 
+			{ fontSize: '48px', fill: '#fff' })
             .setOrigin(0.5);
     }
 
@@ -312,35 +305,22 @@ class Scene1 extends Phaser.Scene {
 				if (this.physics.overlap(tapPoint, note)) {
 					hitPoint = true;
 					note.destroy();
-					//return;
 				}
 			}
-			//console.log('blackDot position:', this.blackDot.x, this.blackDot.y);
-			//console.log('hitPoint: ', hitPoint);
 
             if (hitPoint) {
                 tapPoint.setFillStyle(0x00ff00);
-                //this.score += 10;
             } else {
                 tapPoint.setFillStyle(0xff0000);
 				this.correctScore = 0;
-				//console.log('correctScore', this.correctScore);
             }
 
-            //tapPoint.setVelocityX(300);
-            //this.pointsGroup.add(tapPoint);
-
-			// Проверяем, является ли tapPoint объектом физики перед использованием setVelocityX
 	        /*if (tapPoint.body) {
 	            tapPoint.body.setVelocityX(300);
 	            this.pointsGroup.add(tapPoint);
 	        } else {
 	            console.error("tapPoint не является объектом физики.");
 	        }*/
-
-            // Обновление счета
-            //this.scoreText.setText('Score: ' + this.score * this.correctScore);
-			//console.log('finaleScore', this.score * this.correctScore);
         }
     }
 
